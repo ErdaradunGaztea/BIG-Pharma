aa276bab9d38b9254cef3e7c2ff1baf5b04c71b3
MatiFilozof <matibakala@gmail.com>
2021-01-14 16:25:24 +0100

    added logging to Python test script

--- Python/test_script.py
+++ Python/test_script.py
@@ -1,5 +1,13 @@
 import lyricsgenius
+import logging
 from time import time
+
+logging.basicConfig(format='%(asctime)-15s [%(levelname)s]  %(message)s',
+                    filename="genius.log")
+genius_logger = logging.getLogger('genius_download')
+
+DOWNLOAD_RETRIES = 5
+
 GENIUS_ACCESS_TOKEN = ''
 genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
 
@@ -14,19 +22,25 @@ test_list = ["Jason Derulo", "Taco Hemingway", "Quebonafide", "Klocuch"]
 def save_artists(artists_list):
     for artist in artists_list:
         i=0
-        while i < 5:
+        while i < DOWNLOAD_RETRIES:
+            genius_logger.info("Trying to download data for artist called {0}...".format(artist))
             try:
-                artist_lyrics = genius.search_artist(artist, sort="title", max_songs=10 ,include_features=False, allow_name_change=False)
+                artist_lyrics = genius.search_artist(artist, sort="title", max_songs=10, include_features=False, allow_name_change=False)
             except TypeError:
                 print("Type error, retrying...")
+                genius_logger.warning("Download failed for {0}, {1} tries remaining".format(
+                    artist, DOWNLOAD_RETRIES - i - 1))
                 i += 1
                 continue
             break
-        if i < 5:
+        if i < DOWNLOAD_RETRIES:
+            genius_logger.info("Download for {0} succedded, saving to JSON...".format(artist))
             try:
                 artist_lyrics.save_lyrics("{}.json".format(artist), overwrite=True, ensure_ascii=False)
+                genius_logger.info("Saved {0} to JSON successfully".format(artist))
             except UnicodeEncodeError:
                 print("Unicode error, saving ensuring ascii...")
+                genius_logger.warning("Failed to save {0} to JSON, trying with ASCII...".format(artist))
                 artist_lyrics.save_lyrics("{}.json".format(artist), overwrite=True, ensure_ascii=True)
 
 t1=time()
